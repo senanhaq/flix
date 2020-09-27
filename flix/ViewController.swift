c1b6c6e2c496440a3c6556fe6cd9b47272a4ccec
Senan Haque <senanhaq@gmail.com>
2020-09-27 13:35:29 -0400

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Senan Haque on 9/27/20.
+//  Copyright © 2020 senanhaq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
